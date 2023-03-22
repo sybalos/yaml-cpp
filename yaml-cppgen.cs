f24b338207acaf6b20418176832924575746870a
sybalos <sybalos@gmail.com>
2023-03-22 15:57:37 -0400

    gen

--- yaml-cppgen.cs
+++ yaml-cppgen.cs
@@ -38,26 +38,7 @@ public class YamlCPP : Project
         // dllimport.
         conf.Defines.Add("YAML_CPP_STATIC_DEFINE");
 
-        if (target.OutputType == OutputType.Dll)
-        {
-            // We want this to output a shared library. (DLL)
-            conf.Output = Configuration.OutputType.Dll;
-
-            // This library project expects LIBRARY_DLL symbol to be defined
-            // when used as a DLL. While we could define it in the executable,
-            // it is better to put it as an exported define. That way, any
-            // projects with a dependency on this one will have LIBRARY_DLL
-            // automatically defined by Sharpmake.
-            //conf.ExportDefines.Add("LIBRARY_DLL");
-
-            // Exported defines are not necessarily defines as well, so we need
-            // to add LIBRARY_DLL as an ordinary define too.
-            //conf.Defines.Add("LIBRARY_DLL");
-        }   
-        else if (target.OutputType == OutputType.Lib)
-        {
-            // We want this to output a static library. (LIB)
-            conf.Output = Configuration.OutputType.Lib;
-        }
+        // We want this to output a static library. (LIB)
+        conf.Output = Configuration.OutputType.Lib;
     }
 }
